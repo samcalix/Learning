8242b420cb023dc68614d4fd897d68ba737045c6
andrew-world <samandrew2092@gmail.com>
2026-05-18 21:01:35 +0530

    dont familiar with all data types

---
+++ datatype.py
@@ -0,0 +1,9 @@
+x = "Hello World"    #String
+x = 5                # Integer
+x = 3.14             # Float
+x = complex(0, 89)   # complex
+x= ["apple", "banana", "cherry"]   # List
+x = ("apple", "banana", "cherry")   # Tuple
+x = {"name" : "John", "age" : 30}   # Dictionary
+j = range(6)        # Range
+print(j)
