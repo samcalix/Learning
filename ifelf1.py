77ba73b676b6bcc1ba4089838dabc6ef9e4fc5a3
andrew-world <samandrew2092@gmail.com>
2026-05-20 21:17:47 +0530

    tried even or odd example for the if else

---
+++ ifelf1.py
@@ -0,0 +1,11 @@
+# first write a example code for the if else statement in python then move to the next work 
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+
+test = 12 // 1
+print(test)
